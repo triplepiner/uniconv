ed3f40bb9c2b85e18ae3e1e4a8b82aeffd43c8fd
makar <makul2007@example.com>
2021-11-04 23:24:01 +0300

    my father saved me

--- webapp.py
+++ webapp.py
@@ -14,7 +14,6 @@ import pytube
 
 # We create our webapp
 def webapp():
-    put_markdown('## Welcome to the club buddy')
     #setup the basic layout
     output_box = output()
     put_scrollable(output_box, height=500,keep_bottom=True)
@@ -22,7 +21,7 @@ def webapp():
 
     #collect the file/ yt video
     while True:
-
+#data input
         data = input_group ("File input", [
             file_upload(placeholder="Upload your video file here", multiple=False, max_size='60M',name='source', value=0,accept=['.mp4','.avi','.mov'],),
             input(label='Youtube url',placeholder='Or paste your Youtube url here.', type=URL,name='url'),
@@ -45,7 +44,7 @@ def webapp():
                         select(label='Select the size of your new file that you want relative to the original file.',options=['The Same Size','0.9x','0.5x','0.3x','0.1x'], name='resizer' ),
                         actions(name='last', buttons=['Submit', 'Go back'])
                     ])
-
+#resizing factors
                     if format['resizer'] == 'The Same Size':
                         resize_fac = 1
 
@@ -74,8 +73,12 @@ def webapp():
                             FILE_OUTPUT = 'output.mp4'
                             if os.path.isfile (FILE_OUTPUT):
                                 os.remove (FILE_OUTPUT)
+                            with use_scope('vid_to_mp4'):
+                                put_loading(shape='border',color='info')
                             with open (FILE_OUTPUT, "wb") as out_file:
                                 out_file.write(data['source'].get('content'))
+
+                            clear('vid_to_mp4')
                             output_box.append(put_file(FILE_OUTPUT,content=open('output.mp4', 'rb').read(),label='output.mp4'))
 
 
@@ -85,7 +88,12 @@ def webapp():
                                 os.remove (FILE_OUTPUT)
                             with open (FILE_OUTPUT, "wb") as out_file:
                                 out_file.write(data['source'].get('content'))
+                            with use_scope('vid_to_audio'):
+                                put_loading(shape='border',color='info')
                             vid_to_audio(source=FILE_OUTPUT,resize_factor=resize_fac,export='output.mp3')
+                            clear('vid_to_audio')
+
+
                             output_box.append(put_file('output.mp3',content=open('output.mp3', 'rb').read(),label='output.mp3'))
 
 
@@ -95,15 +103,25 @@ def webapp():
                                 os.remove (FILE_OUTPUT)
                             with open (FILE_OUTPUT, "wb") as out_file:
                                 out_file.write(data['source'].get('content'))
+
+                            with use_scope('vid_to_gif'):
+                                put_loading(shape='border',color='info')
+
                             vid_to_gif(source=FILE_OUTPUT,resize_factor=resize_fac,export='output.gif')
+                            clear('vid_to_gif')
                             output_box.append(put_file('output.gif',content=open('output.gif', 'rb').read(),label='output.gif'))
 
                 if format['last'] == 'Submit' and format['radio'] == 'avi':
                     FILE_OUTPUT = 'output.avi'
                     if os.path.isfile (FILE_OUTPUT):
                         os.remove (FILE_OUTPUT)
+
+                    with use_scope('vid_to_avi'):
+                                put_loading(shape='border',color='info')
+
                     with open (FILE_OUTPUT, "wb") as out_file:
                         out_file.write (data['source'].get ('content'))
+                    clear('vid_to_avi')
                     output_box.append (put_file (FILE_OUTPUT, content=open ('output.avi', 'rb').read (), label='output.avi'))
 
         # format the user wants from yt video
@@ -128,20 +146,28 @@ def webapp():
                 if linker['last'] == 'Go back':
                     run_js ('window.location.reload()')
 
-
+#check if the yt url is valid
             try:
                 if linker['last'] == 'Submit' and linker['radio'] == 'mp4':
                     if os.path.isfile ('output.mp4'):
                         os.remove ('output.mp4')
+                    with use_scope('yt_to_mp4'):
+                       put_loading(shape='border',color='info')
                     yt_to_vid(data['url'],export='output')
+                    clear('yt_to_vid')
                     output_box.append(put_file(name='output.mp4',content=open('output.mp4', 'rb').read(),label='download me!'))
 
                 if linker['last'] == 'Submit' and linker['radio'] == 'mp3':
 
                     if os.path.isfile ('output.mp3'):
                         os.remove ('output.mp3')
+                    with use_scope('vid_to_mp3'):
+                       put_loading(shape='border',color='info')
                     yt_to_audio(data['url'], export='output')
-                    output_box.append (put_file (name='output.mp3', content=open ('output.mp3', 'rb').read (), label='download me!'))
+                    clear('vid_to_mp3')
+                    put_loading(shape='border',color='info')
+
+                    output_box.append (put_file (name='output.mp3', content=open ('output.mp3', 'rb').read()))
 
             except pytube.exceptions.VideoUnavailable:
                 popup ('Invalid url', [
